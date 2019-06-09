b9d8314798e455fe5a9462c563e8a1bbe00d6b02
jaickler <contact@jacobickler.com>
2019-06-09 05:48:59 -0400

    Added the blockchain class with the ability to create a genesis block and also add blocks to itself. Also created unit tests to ensure the blockchain class remains functional.

---
+++ Test_Chain/Test_Chain/blockchain.test.js
@@ -0,0 +1,22 @@
+﻿const Blockchain = require('./blockchain');
+const Block = require('./Block');
+
+describe('Blockchain', () => {
+    let bc;
+
+    beforeEach(() => {
+        bc = new Blockchain();
+    });
+
+    it('start with genesis block', () => {
+        expect(bc.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('adds a new block', () => {
+        const data = 'foo';
+        bc.addBlock(data);
+
+        expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+    });
+});
+
