a2e2ac5947f9af860791a9261aadf671c5ee2842
jaickler <contact@jacobickler.com>
2019-06-09 17:26:16 -0400

    Added the ability to validate chains and replace a short chain with a longer one. Added the appropriate unit tests.

--- Test_Chain/Test_Chain/blockchain.test.js
+++ Test_Chain/Test_Chain/blockchain.test.js
@@ -6,6 +6,7 @@ describe('Blockchain', () => {
 
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('start with genesis block', () => {
@@ -17,6 +18,39 @@ describe('Blockchain', () => {
         bc.addBlock(data);
 
         expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+    });
+
+    it('validates a valid chain', () => {
+        bc2.addBlock('foo');
+
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('invalidates a chain with a corrupt genesis block', () => {
+        bc2.chain[0].data = 'Bad data';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('invalidates a corrupt chain', () => {
+        bc2.addBlock('foo');
+        bc2.chain[1].data = 'Not Foo';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('replaces the chain with a valid chain', () => {
+        bc2.addBlock('Hell bent');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).toEqual(bc2.chain);
+    });
+
+    it('does not replace the chain if length is not sufficient', () => {
+        bc.addBlock('nnnnnnn');
+        bc.replaceChain(bc2.chain);
+
+        expect(bc.chain).not.toEqual(bc2.chain);
     });
 });
 
